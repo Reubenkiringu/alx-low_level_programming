6b44f54065f3169199bd84e970157bc61b51c945
Reubenkiringu <reubenkiringu@gmail.com>
2022-07-03 11:20:04 -0700

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+1 #include <stdio.h>
+2
+3 /**
+4 * main - Entry point
+5 *
+6 * Return:Always 0 (Succcess)
+7 */
+8 int main(void)
+9 {
+10        puts("\"Programming is like building a multilingual puzzle");
+11        return (0);
+12 }
