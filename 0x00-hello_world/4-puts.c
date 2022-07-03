f62e146f30eb4560629cacf19d38127438a77ad3
Reubenkiringu <reubenkiringu@gmail.com>
2022-07-03 11:54:50 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 1   #include <stdio.h>
-2
+2   
 3   /**
-4    * main - Entry point
+4    * main - Entry point 
 5    *
-6    * Return:Always 0 (Succcess)
+6    * Return: Always 0 (success)
 7    */
 8   int main(void)
 9   {
