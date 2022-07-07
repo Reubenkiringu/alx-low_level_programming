46b01e5feb85a47525b1e8a113fe85bc09aa7f20
Reubenkiringu <reubenkiringu@gmail.com>
2022-07-07 06:48:25 -0700

    104 fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,8 +10,8 @@ int main(void)
 int counter, overflow;
 unsigned long a = 1;
 unsigned long b = 1;
-unsigned long sum 0;
-long a_head' a_tail, b_head, b_tail, sum_head, sum_tail;
+unsigned long sum = 0;
+long a_head, a_tail, b_head, b_tail, sum_head, sum_tail;
 printf("1");
 for (counter = 2; counter < 93; counter++) /* will stil print */
 {
