6ee58edff28933f1f761a867d1c9a3753992149a
Reubenkiringu <reubenkiringu@gmail.com>
2022-07-07 03:52:14 -0700

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -34,9 +34,9 @@ _putchar('0' + (product / 10));
 _putchar('0' + (product % 10));
 }
 else if (product >= 100)
-}
+{
 _putchar('0' + (product / 100));
-_putchar{'0' + ((product / 10 % 10));
+_putchar('0' + ((product / 10) % 10));
 _putchar('0' + (product % 10));
 }
 }
