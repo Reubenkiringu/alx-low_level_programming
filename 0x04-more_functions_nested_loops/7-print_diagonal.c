7fd598502ad662f6d3aa7e90632680af8e93443e
Reubenkiringu <reubenkiringu@gmail.com>
2022-07-08 15:35:51 -0700

    parallel

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draw diagonal line o the terminal
+ * @n: numbe of \ to print
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int count, space;
+
+	if (n > 0)
+	{
+		for (count = 0; count < n; count++)
+		{
+			for (space = 0 space < count;
+					space++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			if (count == n - 1)
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
