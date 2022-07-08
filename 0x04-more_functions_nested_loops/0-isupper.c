c5a25bc4be596a3cece9db2925a5ef8055ffd15d
Reubenkiringu <reubenkiringu@gmail.com>
2022-07-08 03:50:16 -0700

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,5 @@
 /*
- * File: o-isupper.c
+ * File: 0-isupper.c
  * Auth: Reuben kiringu
  */
 
@@ -9,7 +9,7 @@
  * _isupper - Checks for uppercase characters.
  * @c: The character to be checked
  *
- * Return: 1 if charcter is uppercase, 0 otherwise
+ * Return: 1 if character is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
