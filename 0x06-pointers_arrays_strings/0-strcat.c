c556fbc0e5ca649ab56a9bd4a6dd632eb217b445
Reubenkiringu <reubenkiringu@gmail.com>
2022-07-13 03:58:29 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,13 +16,13 @@
  */
 char *strcat(char *dest, const char *src)
 {
-	int index + 0, dest_len = 0;
+	int index = 0, dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
 
 	for (index = 0; src[index]; index++)
-		dest[dest _len++] = src[index];
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
