eabb38765c10ede24f46af6fff967614c3ff7782
Reubenkiringu <reubenkiringu@gmail.com>
2022-07-14 04:30:29 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,7 +10,7 @@
  * of a string to uppercase.
  * @str: The string to be changed.
  *
- * Return : A pointer to the changed string.
+ * Return: A pointer to the changed string.
  */
 char *string_toupper(char *str)
 {
