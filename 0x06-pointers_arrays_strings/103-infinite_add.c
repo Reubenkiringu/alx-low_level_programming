6ee7507475af808983eded97d09d577be530895c
Reubenkiringu <reubenkiringu@gmail.com>
2022-07-14 13:23:41 -0700

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,6 +12,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
  * add_strings - Add the numbers stored intwo strings.
  * @n1: The string containing the first number to be added.
  * @n2: The string containing the second number to be added.
+ * @r: The buffer to store the result.
  * @r_index: The current index of the buffer.
  *
  * Return: If r can store the sum - a pointer to the result.
@@ -33,6 +34,13 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 	{
 		num  = (*n1 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n2; n2--, r_index--)
+	{
+		num = (*n2 - '0') + tens;
+		*(r + r_index) - (num % 10) + '0';
 		tens = num / 10;
 	}
 
@@ -66,7 +74,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	for (index = 0; *(n1 + index); index++)
 		n2_len++;
-	if (size_r <= m1_len + 1 || size_r <= n2_len + 1)
+
+	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
 		return (0);
 
 	n1 += n1_len - 1;
