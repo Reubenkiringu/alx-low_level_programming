3d5dc9b0db33ee29866f3dc9ac918b1be7518286
Reubenkiringu <reubenkiringu@gmail.com>
2022-07-14 13:48:21 -0700

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,7 +9,7 @@ char *add_strings(char *n1, char *n2, char *r, int r_index);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 /**
- * add_strings - Add the numbers stored intwo strings.
+ * add_strings - Adds the numbers stored in two strings.
  * @n1: The string containing the first number to be added.
  * @n2: The string containing the second number to be added.
  * @r: The buffer to store the result.
