da3acd2c803a5c5f73ab54c54229ad17c9f3a975
Reubenkiringu <reubenkiringu@gmail.com>
2022-07-19 11:49:11 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,22 +1,24 @@
 #include "main.h"
 
 /**
- * _strchr -> string character
+ * * _strchr -> string character
  * @s: string given
  * @c: another char
  * Return: a string
  */
+
 char *_strchr(char *s, char c)
 {
-	int a = 0, b;
+	int i;
 
-	while (s[a])
-		a++;
-	for (b = 0; b < a; b++)
+	for (i = 0; s[i] >= '\0' ; i++)
 	{
-		if (c == s[b])
-			s += b;
-		return (s);
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+
 	}
+
 	return ('\0');
 }
