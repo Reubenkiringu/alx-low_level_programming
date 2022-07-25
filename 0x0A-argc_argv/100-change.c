1c76f54afa6c3d60393f785a51662bd5aecd9c29
Reubenkiringu <reubenkiringu@gmail.com>
2022-07-25 12:13:33 -0700

    task 5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the minimum number of coins to make change
+ *
+ * @argc: stores number of command line arguments passed
+ * @argv: array of character pointers listing all the arguments
+ *
+ * Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+	while (cents > 0)
+	{
+		coins++;
+		if ((cents - 25) >= 0)
+		{
+			cents = cents - 25;
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
+			cents = cents - 10;
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
+			cents = cents - 2;
+			continue;
+		}
+		cents--;
+	}
+	printf("%d\n", coins);
+	return (0);
+}
