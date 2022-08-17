7158f3d331a3016a7c4689483627174a2fed174b
Reubenkiringu <reubenkiringu98@gmail.com>
2022-08-16 23:52:36 -0700

    task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -33,7 +33,8 @@ list_t *add_node_end(list_t **head, const char *str)
 	else
 	{
 		while (temp->next != NULL)
-			temp->next = new;
+			temp = temp->next;
+		temp->next = new;
 	}
 
 	return (*head);
