664fd9fd531e107a7ea38da57cec3ae50bad4a5c
Reubenkiringu <reubenkiringu98@gmail.com>
2022-08-26 14:03:04 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,9 +3,9 @@
 /**
  * read_textfile - reads a text file and prints the letters
  * @filename: filename.
- * @letters: numbers of the letters printed.
+ * @letters: numbers of letters printed.
  *
- * Return: numbers of the letters printed. It fails, returns 0.
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -13,7 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t nrd, nwr;
 	char *buf;
 
-	if (filename)
+	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
