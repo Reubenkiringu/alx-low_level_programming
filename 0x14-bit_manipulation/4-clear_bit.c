2fee7e10cb923e0cc906c724ace31f788416c154
Reubenkiringu <reubenkiringu98@gmail.com>
2022-09-18 10:49:29 -0700

    task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,7 +8,7 @@
  *
  * Return: 1 if it worked, -1 if it didn't.
  */
-int clear_bit(unsigned lng int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int m;
 
